5f8c933cac8998128be972fb35dd4226c3df41cc
pkkim <paulkimpaul@gmail.com>
2014-10-19 22:20:35 -0400

    Cube was inside-out, fixed it

--- GeneratePolyhedronStl.py
+++ GeneratePolyhedronStl.py
@@ -108,12 +108,12 @@ class PlatonicSolid:
                         Point(0.5, -0.5, 1),
                         Point(-0.5, -0.5, 1),
                         Point(-0.5, 0.5, 1)]
-            faces = [[0, 1, 2, 3],
-                     [4, 5, 6, 7],
-                     [0, 4, 7, 1],
-                     [0, 3, 5, 4],
-                     [1, 7, 6, 2],
-                     [3, 2, 6, 5]]
+            faces = [[0, 3, 2, 1],
+                     [4, 7, 6, 5],
+                     [0, 1, 7, 4],
+                     [0, 4, 5, 3],
+                     [1, 2, 6, 7],
+                     [3, 5, 6, 2]]
             name = "Cube"
         elif shape == PlatonicSolid.Octahedron:
             vertices = [Point(0.5, 0.5, 0),
